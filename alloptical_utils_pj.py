ddcc0ec2d2459292966528ef5e21ce9929e125fd
prajayshah <prajay.shah@mail.utoronto.ca>
2021-04-19 08:00:42 -0400

    moved run_photostim_preprocessing function to alloptical_utils_pj.py and moved some function calls from run_photostim_preprocessing func to the appropriate class init statements for cleaner code hierarchy

--- alloptical_utils_pj.py
+++ alloptical_utils_pj.py
@@ -94,6 +94,11 @@ class TwoPhotonImaging:
             self.suite2p_path = suite2p_path
             self.s2pProcessing(s2p_path=self.suite2p_path)
 
+        # create pkl path and save expobj to pkl object
+        pkl_path = "/home/pshah/mnt/qnap/Analysis/%s/%s_%s/%s_%s.pkl" % (
+        metainfo['date'], metainfo['date'], metainfo['trial'], metainfo['date'], metainfo['trial'])  # specify path in Analysis folder to save pkl object
+        self.save_pkl(pkl_path=pkl_path)
+
     def s2pRun(self, ops, db, user_batch_size):
 
         '''run suite2p on the experiment object, using the attributes deteremined directly from the experiment object'''
@@ -412,7 +417,7 @@ class alloptical(TwoPhotonImaging):
 
     def __init__(self, paths, metainfo, stimtype):
         TwoPhotonImaging.__init__(self, tiff_path_dir=paths[0], tiff_path=paths[1], paq_path=paths[3],
-                                  metainfo=metainfo,
+                                  metainfo=metainfo, analysis_save_path=paths[4],
                                   suite2p_path=None, suite2p_run=False)
         # self.metainfo = metainfo
         self.stim_type = stimtype
@@ -427,7 +432,7 @@ class alloptical(TwoPhotonImaging):
         # self._parsePVMetadata()
 
         ## CREATE THE APPROPRIATE ANALYSIS SUBFOLDER TO USE FOR SAVING ANALYSIS RESULTS TO
-        self.analysis_save_path = self.tiff_path[:21] + 'Analysis/' + self.tiff_path_dir[26:]
+        self.analysis_save_path = paths[4]
         if os.path.exists(self.analysis_save_path):
             pass
         elif os.path.exists(self.analysis_save_path[:-17]):
@@ -436,6 +441,12 @@ class alloptical(TwoPhotonImaging):
             os.mkdir(self.analysis_save_path[:-17])
 
         print('\ninitialized alloptical expobj of exptype and trial: \n', self.metainfo)
+
+        self.stimProcessing(stim_channel='markpoints2packio')
+        self._findTargets()
+        self.find_photostim_frames()
+
+        self.save()
 
     # def _parsePVMetadata(self):
     #
@@ -1693,11 +1704,16 @@ class alloptical(TwoPhotonImaging):
 class Post4ap(alloptical):
     # TODO fix the superclass definitions and heirarchy which might require more rejigging of the code you are running later on as well
 
-    def __init__(self, paths, metainfo, stimtype):
+    def __init__(self, paths, metainfo, stimtype, discard_all):
         alloptical.__init__(self, paths, metainfo, stimtype)
         print('\ninitialized Post4ap expobj of exptype and trial: %s, %s, %s' % (self.metainfo['exptype'],
                                                                                  self.metainfo['trial'],
                                                                                  self.metainfo['date']))
+
+        # collect information about seizures
+        self.collect_seizures_info(seizures_lfp_timing_matarray=paths[5], discard_all=discard_all)
+
+        self.save()
 
     def subselect_tiffs_sz(self, onsets, offsets, on_off_type: str):
         """subselect raw tiff movie over all seizures as marked by onset and offsets. save under analysis path for object.
@@ -2198,6 +2214,78 @@ class OnePhotonStim(TwoPhotonImaging):
         self.lfp_signal = paq['data'][voltage_idx]
 
 
+# main functions used to initiate and run processing of experiments
+# for processing PHOTOSTIM. experiments, creates the all-optical expobj saved in a pkl files at imaging tiff's loc
+def run_photostim_processing(trial, exp_type, tiffs_loc_dir, tiffs_loc, naparms_loc, paqs_loc, pkl_path, metainfo,
+                             new_tiffs, matlab_badframes_path=None, processed_tiffs=True, discard_all=False,
+                             analysis_save_path=''):
+    print('\n-----Processing trial # %s-----' % trial)
+
+    paths = [[tiffs_loc_dir, tiffs_loc, naparms_loc, paqs_loc, analysis_save_path, matlab_badframes_path]]
+    print('tiffs_loc_dir, naparms_loc, paqs_loc paths:\n', paths)
+
+    if 'post' in exp_type and '4ap' in exp_type:
+        expobj = Post4ap(paths[0], metainfo=metainfo, stimtype='2pstim', discard_all=discard_all)
+    else:
+        expobj = alloptical(paths[0], metainfo=metainfo, stimtype='2pstim')
+
+    # for key, values in vars(expobj).items():
+    #     print(key)
+
+    # # these functions are moved to the alloptical class init() location
+    # expobj._parseNAPARMxml()
+    # expobj._parseNAPARMgpl()
+    # expobj._parsePVMetadata()
+    # expobj.stimProcessing(stim_channel='markpoints2packio')
+    # expobj._findTargets()
+    # expobj.find_photostim_frames()
+
+    # # collect information about seizures
+    # if 'post' in exp_type and '4ap' in exp_type:
+    #     expobj.collect_seizures_info(seizures_lfp_timing_matarray=matlab_badframes_path, discard_all=discard_all)
+
+    if len(expobj.bad_frames) > 0:
+        print('***  Collected a total of ', len(expobj.bad_frames),
+              'photostim + seizure/CSD frames +  additional bad frames to bad_frames.npy  ***')
+
+    # if matlab_badframes_path is not None or discard_all is True:
+    #     paq = paq_read(file_path=paqs_loc, plot=False)
+    #     # print(paq[0]['data'][0])  # print the frame clock signal from the .paq file to make sure its being read properly
+    #     bad_frames, expobj.seizure_frames, _, _ = \
+    #         frames_discard(paq=paq[0], input_array=matlab_badframes_path,
+    #                        total_frames=expobj.n_frames, discard_all=discard_all)
+    #     print('\nTotal extra seizure/CSD or other frames to discard: ', len(bad_frames))
+    #     print('|\n -- first and last 10 indexes of these frames', bad_frames[:10], bad_frames[-10:])
+    #     expobj.append_bad_frames(
+    #         bad_frames=bad_frames)  # here only need to append the bad frames to the expobj.bad_frames property
+    #
+    # else:
+    #     expobj.seizure_frames = []
+    #     print('\nNo additional bad (seizure) frames needed for', tiffs_loc_dir)
+    #
+    # if len(expobj.bad_frames) > 0:
+    #     print('***Saving a total of ', len(expobj.bad_frames),
+    #           'photostim + seizure/CSD frames +  additional bad frames to bad_frames.npy***')
+    #     np.save('%s/bad_frames.npy' % tiffs_loc_dir,
+    #             expobj.bad_frames)  # save to npy file and remember to move npy file to tiff folder before running with suite2p
+
+    # Pickle the expobject output to save it for analysis
+
+    # with open(pkl_path, 'wb') as f:
+    #     pickle.dump(expobj, f)
+    # print("\nPkl saved to %s" % pkl_path)
+
+    # make processed tiffs
+    if processed_tiffs:
+        rm_artifacts_tiffs(expobj, tiffs_loc=tiffs_loc, new_tiffs=new_tiffs)
+
+    print('\n----- COMPLETED RUNNING run_photostim_processing() *******')
+    print(metainfo)
+
+    return expobj
+
+
+########
 # preprocessing functions
 # def run_1p_processing(data_path_base, date, animal_prep, trial, metainfo):  # ---> moved to the __init__() for OnePhotonStim class
 #     paqs_loc = '%s/%s_%s_%s.paq' % (
@@ -2865,6 +2953,78 @@ def subselect_tiff(tiff_stack, select_frames, save_as):
     stack8 = convert_to_8bit(stack_cropped)
 
     tf.imwrite(save_as, stack8, photometric='minisblack')
+
+# used for creating tiffs that remove artifacts from alloptical experiments with photostim artifacts
+def rm_artifacts_tiffs(expobj, tiffs_loc, new_tiffs):
+    ### make a new tiff file (not for suite2p) with the first photostim frame whitened, and save new tiff
+    print('\n-----making processed photostim .tiff from:')
+    tiff_path = tiffs_loc
+    print(tiff_path)
+    im_stack = tf.imread(tiff_path, key=range(expobj.n_frames))
+    print('Processing experiment tiff of shape: ', im_stack.shape)
+
+    frames_to_whiten = []
+    for j in expobj.stim_start_frames:
+        frames_to_whiten.append(j)
+
+    # number of photostim frames with artifacts
+    frames_to_remove = []
+    for j in expobj.stim_start_frames:
+        for i in range(0,
+                       expobj.stim_duration_frames + 1):  # usually need to remove 1 more frame than the stim duration, as the stim isn't perfectly aligned with the start of the imaging frame
+            frames_to_remove.append(j + i)
+
+    print('# of total photostim artifact frames:', len(frames_to_remove))
+
+    im_stack_1 = im_stack
+    a = np.full_like(im_stack_1[0], fill_value=0)
+    a[0:100, 0:100] = 5000.
+    for frame in frames_to_whiten:
+        im_stack_1[frame - 3] = im_stack_1[frame - 3] + a
+        im_stack_1[frame - 2] = im_stack_1[frame - 2] + a
+        im_stack_1[frame - 1] = im_stack_1[frame - 1] + a
+    print('Shape', im_stack_1.shape)
+
+    im_stack_1 = np.delete(im_stack_1, frames_to_remove, axis=0)
+    print('After delete shape artifactrem', im_stack_1.shape)
+
+    save_path = (new_tiffs + "_artifactrm.tif")
+    tf.imwrite(save_path, im_stack_1, photometric='minisblack')
+
+    del im_stack_1
+
+    # draw areas on top of im_stack_1 where targets are:
+    im_stack_2 = im_stack
+    print('Shape', im_stack_2.shape)
+
+    for stim in range(expobj.n_groups):
+        b = np.full_like(im_stack_2[0], fill_value=0)
+        targets = expobj.target_areas[stim]
+        for i in np.arange(len(targets)):
+            for j in targets[i]:
+                b[j] = 5000
+
+        all_stim_start_frames = []
+        for stim_frame in expobj.stim_start_frames[stim::expobj.n_groups]:
+            all_stim_start_frames.append(stim_frame)
+        for frame in all_stim_start_frames:
+            #         im_stack_2[frame-4] = im_stack_2[frame-4]+b
+            #         im_stack_2[frame-3] = im_stack_2[frame-3]+b
+            #        im_stack_2[frame-2] = im_stack_2[frame-2]+b
+            im_stack_2[frame - 1] = im_stack_2[frame - 1] + b
+
+    im_stack_2 = np.delete(im_stack_2, expobj.photostim_frames, axis=0)
+
+    print('After delete shape targetcells', im_stack_2.shape)
+
+    save_path = (new_tiffs + '_targetcells.tif')
+    tf.imwrite(save_path, im_stack_2, photometric='minisblack')
+
+    print('done saving to: ', save_path)
+
+    del im_stack_2
+    del im_stack
+
 
 
 # STATISTICS AND OTHER ANALYSES FUNCTIONS
--- run_photostim_preprocessing.py
+++ run_photostim_preprocessing.py
@@ -6,169 +6,10 @@ import tifffile as tf
 import sys
 
 # sys.path.append('/home/pshah/Documents/code/PackerLab_pycharm/')
-import alloptical_utils_pj as ao
+import alloptical_utils_pj as aoutils
 
 
 # from Vape.utils.paq_utils import frames_discard, paq_read
-
-
-# for processing PHOTOSTIM. experiments, creates the all-optical expobj saved in a pkl files at imaging tiff's loc
-
-def rm_artifacts_tiffs(expobj, tiffs_loc, new_tiffs):
-    ### make a new tiff file (not for suite2p) with the first photostim frame whitened, and save new tiff
-    print('\n-----making processed photostim .tiff from:')
-    tiff_path = tiffs_loc
-    print(tiff_path)
-    im_stack = tf.imread(tiff_path, key=range(expobj.n_frames))
-    print('Processing experiment tiff of shape: ', im_stack.shape)
-
-    frames_to_whiten = []
-    for j in expobj.stim_start_frames:
-        frames_to_whiten.append(j)
-
-    # number of photostim frames with artifacts
-    frames_to_remove = []
-    for j in expobj.stim_start_frames:
-        for i in range(0,
-                       expobj.stim_duration_frames + 1):  # usually need to remove 1 more frame than the stim duration, as the stim isn't perfectly aligned with the start of the imaging frame
-            frames_to_remove.append(j + i)
-
-    print('# of total photostim artifact frames:', len(frames_to_remove))
-
-    im_stack_1 = im_stack
-    a = np.full_like(im_stack_1[0], fill_value=0)
-    a[0:100, 0:100] = 5000.
-    for frame in frames_to_whiten:
-        im_stack_1[frame - 3] = im_stack_1[frame - 3] + a
-        im_stack_1[frame - 2] = im_stack_1[frame - 2] + a
-        im_stack_1[frame - 1] = im_stack_1[frame - 1] + a
-    print('Shape', im_stack_1.shape)
-
-    im_stack_1 = np.delete(im_stack_1, frames_to_remove, axis=0)
-    print('After delete shape artifactrem', im_stack_1.shape)
-
-    save_path = (new_tiffs + "_artifactrm.tif")
-    tf.imwrite(save_path, im_stack_1, photometric='minisblack')
-
-    del im_stack_1
-
-    # draw areas on top of im_stack_1 where targets are:
-    im_stack_2 = im_stack
-    print('Shape', im_stack_2.shape)
-
-    for stim in range(expobj.n_groups):
-        b = np.full_like(im_stack_2[0], fill_value=0)
-        targets = expobj.target_areas[stim]
-        for i in np.arange(len(targets)):
-            for j in targets[i]:
-                b[j] = 5000
-
-        all_stim_start_frames = []
-        for stim_frame in expobj.stim_start_frames[stim::expobj.n_groups]:
-            all_stim_start_frames.append(stim_frame)
-        for frame in all_stim_start_frames:
-            #         im_stack_2[frame-4] = im_stack_2[frame-4]+b
-            #         im_stack_2[frame-3] = im_stack_2[frame-3]+b
-            #        im_stack_2[frame-2] = im_stack_2[frame-2]+b
-            im_stack_2[frame - 1] = im_stack_2[frame - 1] + b
-
-    im_stack_2 = np.delete(im_stack_2, expobj.photostim_frames, axis=0)
-
-    print('After delete shape targetcells', im_stack_2.shape)
-
-    save_path = (new_tiffs + '_targetcells.tif')
-    tf.imwrite(save_path, im_stack_2, photometric='minisblack')
-
-    print('done saving to: ', save_path)
-
-    del im_stack_2
-    del im_stack
-
-
-def run_photostim_processing(trial, exp_type, tiffs_loc_dir, tiffs_loc, naparms_loc, paqs_loc, pkl_path, metainfo,
-                             new_tiffs, matlab_badframes_path=None, processed_tiffs=True, discard_all=False,
-                             analysis_save_path=''):
-
-    print('\n-----Processing trial # %s-----' % trial)
-
-    paths = [[tiffs_loc_dir, tiffs_loc, naparms_loc, paqs_loc]]
-    print('tiffs_loc_dir, naparms_loc, paqs_loc paths:\n', paths)
-
-    if 'post' in exp_type and '4ap' in exp_type:
-        expobj = ao.Post4ap(paths[0], metainfo=metainfo, stimtype='2pstim')
-    elif 'pre' in exp_type and '4ap' in exp_type:
-        expobj = ao.alloptical(paths[0], metainfo=metainfo, stimtype='2pstim')
-    else:
-        expobj = ao.TwoPhotonImaging()
-
-    # for key, values in vars(expobj).items():
-    #     print(key)
-
-    # expobj._parseNAPARMxml()
-    # expobj._parseNAPARMgpl()
-    # expobj._parsePVMetadata()
-    expobj.stimProcessing(stim_channel='markpoints2packio')
-    expobj._findTargets()
-    expobj.find_photostim_frames()
-
-    # set analysis save path for expobj
-    # make the necessary Analysis saving subfolder as well
-    expobj.analysis_save_path = analysis_save_path
-    if os.path.exists(expobj.analysis_save_path):
-        pass
-    elif os.path.exists(expobj.analysis_save_path[:-17]):
-        os.mkdir(expobj.analysis_save_path)
-    elif os.path.exists(expobj.analysis_save_path[:-27]):
-        os.mkdir(expobj.analysis_save_path[:-17])
-        os.mkdir(expobj.analysis_save_path)
-
-    expobj.save_pkl(pkl_path)
-    # with open(pkl_path, 'wb') as f:
-    #     pickle.dump(expobj, f)
-    # print("\nPkl saved to %s" % pkl_path)
-
-    # collect information about seizures
-    if 'post' in exp_type and '4ap' in exp_type:
-        expobj.collect_seizures_info(seizures_lfp_timing_matarray=matlab_badframes_path, discard_all=discard_all)
-
-    if len(expobj.bad_frames) > 0:
-        print('***  Collected a total of ', len(expobj.bad_frames),
-              'photostim + seizure/CSD frames +  additional bad frames to bad_frames.npy  ***')
-
-    # if matlab_badframes_path is not None or discard_all is True:
-    #     paq = paq_read(file_path=paqs_loc, plot=False)
-    #     # print(paq[0]['data'][0])  # print the frame clock signal from the .paq file to make sure its being read properly
-    #     bad_frames, expobj.seizure_frames, _, _ = \
-    #         frames_discard(paq=paq[0], input_array=matlab_badframes_path,
-    #                        total_frames=expobj.n_frames, discard_all=discard_all)
-    #     print('\nTotal extra seizure/CSD or other frames to discard: ', len(bad_frames))
-    #     print('|\n -- first and last 10 indexes of these frames', bad_frames[:10], bad_frames[-10:])
-    #     expobj.append_bad_frames(
-    #         bad_frames=bad_frames)  # here only need to append the bad frames to the expobj.bad_frames property
-    #
-    # else:
-    #     expobj.seizure_frames = []
-    #     print('\nNo additional bad (seizure) frames needed for', tiffs_loc_dir)
-    #
-    # if len(expobj.bad_frames) > 0:
-    #     print('***Saving a total of ', len(expobj.bad_frames),
-    #           'photostim + seizure/CSD frames +  additional bad frames to bad_frames.npy***')
-    #     np.save('%s/bad_frames.npy' % tiffs_loc_dir,
-    #             expobj.bad_frames)  # save to npy file and remember to move npy file to tiff folder before running with suite2p
-
-    # Pickle the expobject output to save it for analysis
-
-    with open(pkl_path, 'wb') as f:
-        pickle.dump(expobj, f)
-    print("\nPkl saved to %s" % pkl_path)
-
-    # make processed tiffs
-    if processed_tiffs:
-        rm_artifacts_tiffs(expobj, tiffs_loc=tiffs_loc, new_tiffs=new_tiffs)
-
-    print('\n----- COMPLETED RUNNING run_photostim_processing() *******')
-    print(metainfo)
-
 
 # %% update the trial and photostim experiment files information below before running run_photostim_processing()
 data_path_base = '/home/pshah/mnt/qnap/Data/2021-01-19'
@@ -207,7 +48,7 @@ metainfo = {
     'comments': comments
 }
 
-run_photostim_processing(trial, exp_type=exp_type, pkl_path=pkl_path, new_tiffs=new_tiffs, metainfo=metainfo,
+expobj = aoutils.run_photostim_processing(trial, exp_type=exp_type, pkl_path=pkl_path, new_tiffs=new_tiffs, metainfo=metainfo,
                          tiffs_loc_dir=tiffs_loc_dir, tiffs_loc=tiffs_loc, naparms_loc=(data_path_base+naparms_loc),
                          paqs_loc=paqs_loc, matlab_badframes_path=matlab_badframes_path,
                          processed_tiffs=False, discard_all=True, analysis_save_path=analysis_save_path)
