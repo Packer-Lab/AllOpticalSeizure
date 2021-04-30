e1a8c9f318e75ec464a8b521693ad243e6f3439c
prajayshah <prajay.shah@mail.utoronto.ca>
2021-04-30 09:32:40 -0400

    testing changed mapping

--- alloptical_misc.py
+++ alloptical_misc.py
@@ -5,9 +5,6 @@ trial = 't-011'
 date = '2021-01-11'
 pj.SaveDownsampledTiff(tiff_path="/home/pshah/mnt/qnap/Data/%s/%s_%s/%s_%s_Cycle00001_Ch3.tif" % (date, date, trial, date, trial))
 
-
-# to download downsampled: -01-10 t-014 (look for seizure, compared to paq); 01-08 t-012,
-
 #%% PLOT THE ZPROFILE OF A TIFF STACK
 trial = 't-015'
 date = '2021-01-19'
