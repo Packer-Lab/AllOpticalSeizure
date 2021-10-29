bug fixes in: plot_periphotostim_avg2 function that is more useful for plotting of final results for peristim Flu averages..

alloptical_plotting_utils.py
@@ -333,8 +333,8 @@ def plot_photostim_traces_overlap(array, expobj, exclude_id=[], y_spacing_factor
 
 
 ### photostim analysis - PLOT avg over photostim. trials traces for the provided traces
-def plot_periphotostim_avg2(dataset, fps=None, legend_labels=None, colors=None, xlabel='Frames',ylabel='dF/stdF', avg_with_std=False,
-                            title=None, pre_stim_sec=None, **kwargs):
+def plot_periphotostim_avg2(dataset, fps=None, legend_labels=None, colors=None, xlabel='Frames',ylabel='ylabel', avg_with_std=False,
+                            title='high quality plot', pre_stim_sec=None, ylim=None, **kwargs):
 
     if 'fig' in kwargs.keys():
         fig = kwargs['fig']
@@ -361,8 +361,10 @@ def plot_periphotostim_avg2(dataset, fps=None, legend_labels=None, colors=None, 
             std = np.std(dataset[i], axis=0, ddof=1)
             meantraces.append(meanst)
             stdtraces.append(std)
-            assert meanst.shape == meantraces[i - 1].shape, print('length mismatch in mean traces of datasets...')
-            assert std.shape == stdtraces[i - 1].shape, print('length mismatch in std traces of datasets...')
+            if not meanst.shape == meantraces[i - 1].shape:
+                print(f"|--- length mismatch in mean traces of datasets...{title}")
+            if not std.shape == stdtraces[i - 1].shape:
+                print(f"|--- length mismatch in std traces of datasets...{title}")
 
     elif type(dataset) is not list or len(dataset) == 1:
         dataset = list(dataset)
@@ -372,36 +374,43 @@ def plot_periphotostim_avg2(dataset, fps=None, legend_labels=None, colors=None, 
         stdtraces.append(std)
         colors = ['black']
     else:
-        ReferenceError('please provide the data to plot in a list format, each different data group as a list item...')
+        AttributeError('please provide the data to plot in a list format, each different data group as a list item...')
 
 
     if 'time' in xlabel or 'Time' in xlabel:
         ## change xaxis to time (secs)
-        if fps:
-            if pre_stim_sec:
-                x_range = np.linspace(0, len(meanst[0]) / fps, len(
-                    meanst)) - pre_stim_sec  # x scale, but in time domain (transformed from frames based on the provided fps)
+        if fps is not None:
+            if pre_stim_sec is not None:
+                x_range = np.linspace(0, len(meantraces[0]) / fps, len(
+                    meantraces[0])) - pre_stim_sec  # x scale, but in time domain (transformed from frames based on the provided fps)
                 ax.set_xlabel('Time post stim (secs)')
             else:
-                ReferenceError('need to provide a pre_stim_sec value to the function call!')
+                AttributeError('need to provide a pre_stim_sec value to the function call!')
         else:
-            ReferenceError('need to provide fps value to convert xaxis in units of time (secs)')
+            AttributeError('need to provide fps value to convert xaxis in units of time (secs)')
     else:
         x_range = range(len(meanst[0]))
 
     for i in range(len(meantraces)):
         if avg_with_std:
+            if len(meantraces[i]) < len(x_range):  ## TEMP FIX
+                mismatch = (len(x_range) - len(meantraces[i]))
+                meantraces[i] = np.append(meantraces[i], [0] * mismatch)
+                stdtraces[i] = np.append(stdtraces[i], [0] * mismatch)
+                print(f'|------ adding {mismatch} zeros to mean and std-fill traces to make the arrays the same length, new length of plot array: {meantraces[i].shape} ')
+
             ax.plot(x_range, meantraces[i], color=colors[i], lw=2)
             ax.fill_between(x_range, meantraces[i] - stdtraces[i], meantraces[i] + stdtraces[i], alpha=0.15, color=colors[i])
         else:
             ax.plot(x_range, meantraces[i], color=colors[i], lw=2)
             for trace in dataset[i]:
-                ax.plot(trace, color=colors[i], alpha=0.3, lw=2)
+                ax.plot(x_range, trace, color=colors[i], alpha=0.3, lw=2)
 
 
     if legend_labels:
         ax.legend(legend_labels)
-    ax.set_ylim([-0.5, 1.0])
+    if ylim:
+        ax.set_ylim([ylim[0],ylim[1]])
     ax.set_ylabel(ylabel)
     ax.set_title(title)
 
@@ -558,7 +567,7 @@ def plot_flu_trace(expobj, cell, x_lims=None, slm_group=None, to_plot='raw', fig
         to_plot_ = raw_dff
         to_thresh = std_dff
     else:
-        ValueError('specify to_plot as either "raw" or "dff"')
+        AttributeError('specify to_plot as either "raw" or "dff"')
 
     # make the plot either as just the raw trace or as a dFF trace with the std threshold line drawn as well.
     plt.figure(figsize=figsize)

alloptical_results_photostim.py
@@ -37,6 +37,7 @@ for exp in experiments:
 
             mean_pre4ap_ = allopticalResults.possig_responders_traces[row][0]
             mean_post4ap_ = allopticalResults.possig_responders_traces[row][1]
+            print(mean_pre4ap_.shape, mean_post4ap_.shape)
 
             # plot avg with confidence intervals
             # fig, ax = plt.subplots()
@@ -44,41 +45,52 @@ for exp in experiments:
             ax = axs[counter//ncols, counter % ncols]
 
             meanst = np.mean(mean_pre4ap_, axis=0)
-            std = np.std(mean_pre4ap_, axis=0, ddof=1)
             ## change xaxis to time (secs)
             if len(meanst) < 100:
                 fps = 15
             else:
                 fps = 30
-            x_time = np.linspace(0, len(meanst) / fps, len(meanst)) - allopticalResults.pre_stim_sec  # x scale, but in time domain (transformed from frames based on the provided fps)
-            ax.set_xlabel('Time post stim (secs)')
 
-            ax.plot(x_time, meanst, color='black', lw=2)
-            ax.fill_between(x_time, meanst - std, meanst + std, alpha=0.15, color='gray')
+            aoplot.plot_periphotostim_avg2(dataset=[mean_pre4ap_, mean_post4ap_], fps=fps, legend_labels=[f"pre4ap {mean_pre4ap_.shape[0]} cells", f"post4ap {mean_post4ap_.shape[0]} cells"],
+                                           colors=['black', 'green'], xlabel='Time post stim (secs)', ylabel='dF/stdF', avg_with_std=True, title=f"{allopticalResults.num_sig_responders_df.index[row]}", ylim=[-0.5, 1.0],
+                                           pre_stim_sec=allopticalResults.pre_stim_sec, fig=fig, ax=ax, show=False)
 
-            meanst = np.mean(mean_post4ap_, axis=0)
-            std = np.std(mean_post4ap_, axis=0, ddof=1)
-            ## change xaxis to time (secs)
-            if len(meanst) < 100:
-                fps = 15
-            else:
-                fps = 30
-            x_time = np.linspace(0, len(meanst) / fps, len(meanst)) - allopticalResults.pre_stim_sec  # x scale, but in time domain (transformed from frames based on the provided fps)
-
-            ax.plot(x_time, meanst, color='green', lw=2)
-            ax.fill_between(x_time, meanst - std, meanst + std, alpha=0.15, color='green')
-
-            ax.legend([f"pre4ap {mean_pre4ap_.shape[0]} cells", f"post4ap {mean_post4ap_.shape[0]} cells"])
-            ax.set_ylim([-0.5, 1.0])
-            ax.set_ylabel('dF/pre_stdF')
-            ax.set_title(f"{allopticalResults.num_sig_responders_df.index[row]}")
+            # meanst = np.mean(mean_pre4ap_, axis=0)
+            # std = np.std(mean_pre4ap_, axis=0, ddof=1)
+            # ## change xaxis to time (secs)
+            # if len(meanst) < 100:
+            #     fps = 15
+            # else:
+            #     fps = 30
+            # x_time = np.linspace(0, len(meanst) / fps, len(meanst)) - allopticalResults.pre_stim_sec  # x scale, but in time domain (transformed from frames based on the provided fps)
+            # ax.set_xlabel('Time post stim (secs)')
+            #
+            # ax.plot(x_time, meanst, color='black', lw=2)
+            # ax.fill_between(x_time, meanst - std, meanst + std, alpha=0.15, color='gray')
+            #
+            # meanst = np.mean(mean_post4ap_, axis=0)
+            # std = np.std(mean_post4ap_, axis=0, ddof=1)
+            # ## change xaxis to time (secs)
+            # if len(meanst) < 100:
+            #     fps = 15
+            # else:
+            #     fps = 30
+            # x_time = np.linspace(0, len(meanst) / fps, len(meanst)) - allopticalResults.pre_stim_sec  # x scale, but in time domain (transformed from frames based on the provided fps)
+            #
+            # ax.plot(x_time, meanst, color='green', lw=2)
+            # ax.fill_between(x_time, meanst - std, meanst + std, alpha=0.15, color='green')
+            #
+            # ax.legend([f"pre4ap {mean_pre4ap_.shape[0]} cells", f"post4ap {mean_post4ap_.shape[0]} cells"])
+            # ax.set_ylim([-0.5, 1.0])
+            # ax.set_ylabel('dF/pre_stdF')
+            # ax.set_title(f"{allopticalResults.num_sig_responders_df.index[row]}")
 
             counter += 1
 fig.suptitle('Avg. positive responders')
 fig.show()
 
 
-# negative responders
+# %% negative responders
 ncols = 3
 nrows = 3
 fig, axs = plt.subplots(ncols=ncols, nrows=nrows, figsize=(12, 14))

alloptical_utils_pj.py
@@ -2361,7 +2361,7 @@ class alloptical(TwoPhotonImaging):
                                 trace_dff = [np.nan] * len(trace)
                                 dFstdF = [np.nan] * len(trace)
                         else:
-                            ReferenceError(
+                            AttributeError(
                                 'no cells_sz_stim attr, so classify cells in sz boundary hasnot been saved for this expobj')
 
                     flu_dff.append(trace_dff)
@@ -3873,7 +3873,7 @@ def get_nontargets_stim_traces_norm(expobj, normalize_to='', pre_stim=10, post_s
                             trace_dff = [np.nan] * len(trace)
                             dFstdF = [np.nan] * len(trace)
                     else:
-                        ReferenceError('no cells_sz_stim attr, so classify cells in sz boundary hasnot been saved for this expobj')
+                        AttributeError('no cells_sz_stim attr, so classify cells in sz boundary hasnot been saved for this expobj')
 
                 flu_dfstdF.append(dFstdF)
                 flu_dff.append(trace_dff)