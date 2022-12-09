6fee30fb6b785a0539cb70fd7e46376f9793d9c7
prajayshah <prajay.shah@mail.utoronto.ca>
2022-12-09 10:06:37 -0500

    swapped fig 5 <--> fig 6

--- Figures/figure6-RF.py
+++ Figures/figure6-RF.py
@@ -1,43 +1,63 @@
 """
 TODO:
 
-RUN STATS:
-[x] - ttests for linear regression ratios
+[x] add schematic of seizure distance to target
+
+STATS:
+[x] repsonses vs. time to invasion
+
 
 suppl figure: write up RF code
-- responders analysis
+- seizure boundary classification
+- responses vs. pre-stim Flu targets annulus, across all conditions
+    + stats tests on the averages? or can tests be carried out directly on the 2-D density plots?
 
 """
 
 # %%
 import sys
 
+from _analysis_._ClassTargetsSzInvasionTemporal import TargetsSzInvasionTemporal, TargetsSzInvasionTemporalResults
+from _analysis_.run__TargetsSzInvasionTemporal import plot__targets_sz_invasion_meantraces
+
 sys.path.extend(['/home/pshah/Documents/code/reproducible_figures-main'])
 
 import rep_fig_vis as rfv
+from _utils_.rfv_funcs import make_fig_layout, show_test_figure_layout, add_label_axes
 import alloptical_utils_pj as Utils
-import numpy as np
-
-import funcsforprajay.funcs as pj
+from _analysis_._ClassPhotostimAnalysisSlmTargets import PhotostimAnalysisSlmTargets
 
 from _analysis_.nontargets_analysis._ClassPhotostimResponsesAnalysisNonTargets import \
     PhotostimResponsesAnalysisNonTargets
-from _analysis_.nontargets_analysis._ClassResultsNontargetPhotostim import PhotostimResponsesNonTargetsResults
+
+import funcsforprajay.plotting as pplot
+import funcsforprajay.funcs as pj
+
+import numpy as np
+import matplotlib.image as mpimg
+
+from _analysis_._ClassTargetsSzInvasionSpatial_codereview import TargetsSzInvasionSpatial_codereview, \
+    TargetsSzInvasionSpatialResults_codereview
+
+SAVE_FIG = "/home/pshah/Documents/figures/alloptical-photostim-responses-sz-distance/"
+
+main_spatial = TargetsSzInvasionSpatial_codereview
+results_spatial = TargetsSzInvasionSpatialResults_codereview.load()
+
+main_temporal = TargetsSzInvasionTemporal
+results_temporal = TargetsSzInvasionTemporalResults.load()
+
 from _utils_.alloptical_plotting import plot_settings
-
-
-main = PhotostimResponsesAnalysisNonTargets
-
-results: PhotostimResponsesNonTargetsResults = PhotostimResponsesNonTargetsResults.load()
 
 plot_settings()
 SAVE_FOLDER = f'/home/pshah/Documents/figures/alloptical_seizures_draft/'
 
 # %% SETUP
 ## Set general plotting parameters
-rfv.set_fontsize(8)
+fontsize = 8
+fs = fontsize
+rfv.set_fontsize(fs)
 
-## Set parameters
 save_fig = True
 
 np.random.seed(2)  # fix seed
@@ -48,167 +68,83 @@ np.random.seed(2)  # fix seed
 # bound = l, b, r, t
 
 layout = {
-    'left': {'panel_shape': (1, 1),
-                      'bound': (0.10, 0.80, 0.25, 0.95)},
-    'middle-left': {'panel_shape': (1, 2),
-                         'bound': (0.37, 0.70, 0.52, 0.95)},
-    'middle-right': {'panel_shape': (1, 2),
-                           'bound': (0.59, 0.72, 0.64, 0.95),
+    'main-left-top': {'panel_shape': (1, 1),
+                      'bound': (0.05, 0.77, 0.27, 0.92)},
+    'main-left-bottom': {'panel_shape': (1, 1),
+                         'bound': (0.08, 0.42, 0.26, 0.57)},
+    'main-right-tophigh': {'panel_shape': (1, 1),
+                           'bound': (0.40, 0.75, 0.90, 0.95),
                            'wspace': 0.8},
-    'right': {'panel_shape': (1, 1),
-                          'bound': (0.75, 0.70, 0.95, 0.85),
-                          'wspace': 0.8}
+    'main-right-toplow': {'panel_shape': (1, 1),
+                          'bound': (0.40, 0.66, 0.90, 0.71),
+                          'wspace': 0.8},
+    'main-right-bottomhigh': {'panel_shape': (1, 1),
+                              'bound': (0.40, 0.40, 0.90, 0.60),
+                              'wspace': 0.8},
+    'main-right-bottomlow': {'panel_shape': (1, 1),
+                             'bound': (0.40, 0.31, 0.90, 0.34),
+                             'wspace': 0.8},
 }
-
 dpi = 300
 fig, axes, grid = rfv.make_fig_layout(layout=layout, dpi=dpi)
 
 # rfv.show_test_figure_layout(fig, axes=axes)  # test what layout looks like quickly, but can also skip and moveon to plotting data.
 
-
-# %% MAKE PLOTS
 x_adj = 0.09
 
-# %% B) total z scored responses of targets vs. total z scored responses of nontargets - photostim vs. sham stim
-axs = (axes['middle-left'], axes['middle-right'])
-rfv.add_label_axes(text='B', ax=axs[0][0], x_adjust=x_adj, y_adjust=0.00)
 
-# main.collect__zscored_summed_activity_vs_targets_activity(results=results)
+# %% A' - photostim responses relative to distance to seizure
+ax = axes['main-right-tophigh'][0]
 
-# B + B') ratio of regression lines between baseline and interictal stims - photostim + fakestim
-main.plot__summed_activity_vs_targets_activity(results=results, SAVE_FOLDER=SAVE_FOLDER, fig=fig, axs=axs)
+# rfv.add_label_axes(text="A'", ax=ax, x_adjust=x_adj + 0.03)
+main_spatial.collect__binned__distance_v_responses(results=results_spatial, rerun=0)
+
+# TRYING ROLLING BINS - AUG 11 2022:
+main_spatial.collect__binned__distance_v_responses_rolling_bins(results=results_spatial, rerun=0)
+
+results_spatial = TargetsSzInvasionSpatialResults_codereview.load()
+# main_spatial.plot__responses_v_distance_no_normalization(results=results_spatial, axes=(axes['main-right-tophigh'], axes['main-right-toplow']), fig=fig)
+main_spatial.plot__responses_v_distance_no_normalization_rolling_bins(results=results_spatial, axes=(axes['main-right-tophigh'], axes['main-right-toplow']), fig=fig)
+# ax.text(x=50, y=2, s=f'{results_spatial.binned__distance_vs_photostimresponses["kruskal - binned responses"]}', fontsize=5)
+# ax.text(x=50, y=1.95, s=f'{results_spatial.binned__distance_vs_photostimresponses["anova oneway - binned responses"]}', fontsize=5)
 
 
 
-# %% legend for B and C plots
+# %% B' - photostim responses relative to time to seizure recruitment
+ax = axes['main-right-bottomhigh'][0]
+# rfv.add_label_axes(text="B'", ax=ax, x_adjust=x_adj + 0.03)
+results_temporal = main_temporal.collect__binned__szinvtime_v_responses(results=results_temporal, rerun=0, bin_width=0.5)  # binsize = .1 secs
+main_temporal.plot__responses_v_szinvtemporal_no_normalization(results=results_temporal, save_path_full=f'{SAVE_FIG}/responses_sz_temporal_binned_line_plot.png',
+                                                               axes=(axes['main-right-bottomhigh'], axes['main-right-bottomlow']), fig=fig)
+# ax.text(x=-2, y=2, s=f'{results_temporal.binned__time_vs_photostimresponses["kruskal - binned responses"]}', fontsize=5)
+ax.text(x=-2, y=1.95, s=f'{results_temporal.binned__time_vs_photostimresponses["anova oneway - binned responses"]}', fontsize=5)
 
-# Create the legend
-from matplotlib.lines import Line2D
-from matplotlib.transforms import Bbox
 
-legend_elements = [Line2D([0], [0], marker='o', color='royalblue', label='Baseline', lw=2,
-                          markerfacecolor='royalblue', markersize=7, markeredgecolor='black'),
-                   Line2D([0], [0], marker='o', color='forestgreen', label='Interictal', lw=2,
-                          markerfacecolor='forestgreen', markersize=7, markeredgecolor='black'),
-                   Line2D([0], [0], marker='o', color='gray', label='Artificial stimulation', lw=2,
-                          markerfacecolor='white', markersize=7, markeredgecolor='black'),
-                   ]
+# %% MAKE PLOTS
 
-bbox = np.array(axes['right'][0].get_position())
-bbox = Bbox.from_extents(bbox[0, 1] + 0.02, bbox[1, 1] + 0.15, bbox[1, 0], bbox[1, 1])
-# bbox = Bbox.from_extents(bbox[1, 0], bbox[0, 0], bbox[0, 1] + 0.00, bbox[0, 1] - 0.02)
-# ax2 = fig.add_subplot(position = bbox)
-axlegend = fig.add_subplot()
-axlegend.set_position(pos=bbox)
-axlegend.legend(handles=legend_elements, loc='center')
-axlegend.axis('off')
+# %% A - schematic of sz distance to target
+ax = axes['main-left-top'][0]
+rfv.add_label_axes(text='A', ax=ax, x_adjust=x_adj - 0.06)
+sch_path = '/home/pshah/Documents/figures/alloptical_seizures_draft/figure-items/schematic-targets-distance-to-sz.png'
+img = mpimg.imread(sch_path)
+axes['main-left-top'][0].imshow(img, interpolation='none')
+axes['main-left-top'][0].axis('off')
+# axes['main-left-top'][0].set_title('Distance to seizure boundary')
+
+# %% B - Flu change during recruitment to seizure of target
+ax = axes['main-left-bottom'][0]
+rfv.add_label_axes(text="B", ax=ax, x_adjust=x_adj - 0.02)
+plot__targets_sz_invasion_meantraces(fig=fig, ax=axes['main-left-bottom'][0])
+# axes['main-left-bottom'][0].set_title('Time to seizure recruitment')
 # fig.show()
-
-# %% C) influence measurements - baseline + interictal across distance to targets
-# PLOTTING of average responses +/- sem across distance to targets bins - baseline + interictal
-axs = axes['right']
-rfv.add_label_axes(text='C', ax=axs[0], x_adjust=x_adj - 0.00)
-
-distance_lims = [19, 400]
-
-measurement = 'new influence response'
-ax = axs[0]
-ax.axhline(y=0, ls='--', color='gray', lw=1.5)
-ax.axvline(x=20, ls='--', color='gray', lw=1.5)
-
-# BASELINE- distances vs. responses
-distances = results.binned_distance_vs_responses[measurement]['distances']
-distances_lim_idx = [idx for idx, distance in enumerate(distances) if distance_lims[0] < distance < distance_lims[1]]
-distances = distances[distances_lim_idx]
-avg_binned_responses = results.binned_distance_vs_responses[measurement]['avg binned responses'][distances_lim_idx]
-sem_binned_responses = results.binned_distance_vs_responses[measurement]['sem binned responses'][distances_lim_idx]
-ax.fill_between(x=list(distances), y1=list(avg_binned_responses + sem_binned_responses), y2=list(avg_binned_responses - sem_binned_responses), alpha=0.3, color='royalblue')
-ax.plot(distances, avg_binned_responses, lw=1.5, color='royalblue', label='baseline')
-
-
-# binned distances vs responses
-distances = results.binned_distance_vs_responses_interictal[measurement]['distances']
-distances_lim_idx = [idx for idx, distance in enumerate(distances) if distance_lims[0] < distance < distance_lims[1]]
-distances = distances[distances_lim_idx]
-avg_binned_responses = results.binned_distance_vs_responses_interictal[measurement]['avg binned responses'][distances_lim_idx]
-sem_binned_responses = results.binned_distance_vs_responses_interictal[measurement]['sem binned responses'][distances_lim_idx]
-ax.fill_between(x=list(distances), y1=list(avg_binned_responses + sem_binned_responses), y2=list(avg_binned_responses - sem_binned_responses), alpha=0.3, color='mediumseagreen')
-ax.plot(distances, avg_binned_responses, lw=1.5, color='mediumseagreen', label='interictal')
-
-ax.set_title(f"{measurement}", wrap=True)
-ax.set_xlim([0, 400])
-ax.set_ylim([-0.175, 0.25])
-pj.lineplot_frame_options(fig=fig, ax=ax, x_label='Distance to target ($\mu$$\it{m}$)', y_label='Photostimulation influence')
-# ax.legend(loc='lower right')
-# fig.suptitle('BASELINE + INTERICTAL')
-# fig.tight_layout()
-# fig.show()
-
-## TWO WAY ANOVA
-# build longform table for TWO-WAY ANOVA - baseline vs. interictal
-
-#
-# distance_responses_df = pd.DataFrame({
-#     'group': [],
-#     'distance': [],
-#     'response': []
-# })
-#
-# distance_response = results.binned_distance_vs_responses['new influence response']['distance responses']
-#
-# # distance_responses_df = []
-# for distance, responses in distance_response.items():
-#     if distance_lims[0] < distance < distance_lims[1]:
-#         for response in responses:
-#             _df = pd.DataFrame({
-#                 'group': 'baseline',
-#                 'distance': distance,
-#                 'response': response
-#             },  index=[f"baseline_{distance}"])
-#             distance_responses_df = pd.concat([distance_responses_df, _df])
-#
-#
-# distance_response = results.binned_distance_vs_responses_interictal['new influence response']['distance responses']
-#
-# for distance, responses in distance_response.items():
-#     if distance_lims[0] < distance < distance_lims[1]:
-#         for response in responses:
-#             _df = pd.DataFrame({
-#                 'group': 'interictal',
-#                 'distance': distance,
-#                 'response': response
-#             },  index=[f"interictal_{distance}"])
-#             distance_responses_df = pd.concat([distance_responses_df, _df])
-#
-#
-#
-# # perform TWO WAY ANOVA
-#
-# model = ols('response ~ C(group) + C(distance) + C(group):C(distance)', data=distance_responses_df).fit()
-# sm.stats.anova_lm(model, typ=2)
-
-
-# %% A - num targets at each distance - split by individual experiments
-ax = axes['left'][0]
-rfv.add_label_axes(text='A', ax=ax, x_adjust=x_adj, y_adjust=0.00)
-
-baseline_responses = results.baseline_responses.iloc[results.pre4ap_idxs]
-# num occurrences at each distance - split by trial types
-distances = []
-for exp in np.unique(baseline_responses['expID']):
-    _distances = list(baseline_responses[baseline_responses['expID'] == exp]['distance target'])
-    distances.append(_distances)
-ax.hist(distances, 40, density=False, histtype='bar', stacked=True)
-# ax.set_title('number of measurements by individual experiments')
-ax.set_xlabel('Distance to target ($\mu$$\it{m}$)')
 
 
 
 # %%
-if save_fig and dpi > 250:
+if save_fig and dpi >= 250:
     Utils.save_figure(fig=fig, save_path_full=f"{SAVE_FOLDER}/figure6-RF.png")
     Utils.save_figure(fig=fig, save_path_full=f"{SAVE_FOLDER}/figure6-RF.svg")
 
-fig.show()
+# fig.show()
 
 
